6b0402b4c38bd95cd82994e0e1282a5677e50495
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-06-19 10:40:12 +0200

    chore: let update script create the PR

--- scripts/update-cdktf.sh
+++ scripts/update-cdktf.sh
@@ -6,7 +6,18 @@ PROJECT_ROOT=$(cd "$(dirname "${BASH_SOURCE:-$0}")/.." && pwd)
 CDKTF_VERSION=$1
 
 echo "Updating to cdktf version $CDKTF_VERSION"
+
+git checkout -b cdktf-update-$CDKTF_VERSION
+
 cd $PROJECT_ROOT/infrastructure
 
 yarn add -D cdktf-cli@$CDKTF_VERSION
 yarn add cdktf@$CDKTF_VERSION @cdktf/provider-aws@latest @cdktf/provider-null@latest
+
+git add --all
+
+git commit -m "chore: update cdktf to $CDKTF_VERSION"
+git push origin cdktf-update-$CDKTF_VERSION
+
+gh pr create -f --base main --head cdktf-update-$CDKTF_VERSION --title "chore: update cdktf to $CDKTF_VERSION" --label "cdktf-update-$CDKTF_VERSION"
+
